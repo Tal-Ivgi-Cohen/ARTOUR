8e1419ccd5ee5a8a7230dd499fc5c520d07438eb
Tal-Ivgi <tal.ivgi@gmail.com>
2021-07-11 21:53:48 +0300

    footer

--- Frontend/src/cmps/util/Footer.jsx
+++ Frontend/src/cmps/util/Footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-//import { Fragment } from 'react'
 export class Footer extends React.Component {
 
     render() {
         return (
             <footer className="app-footer">
                 <p>coffeerights ARTOUR Marketplace &copy; </p>
-                <p>By Dana Sandman & Gal Golikov & Tal Ivgi </p>
             </footer>
         )
     }
