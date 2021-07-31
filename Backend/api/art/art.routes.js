3bd7d9b49201c13b97117b638eeb2dc8ea26231b
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-07-31 14:53:19 +0300

    Update art.routes.js

--- Backend/api/art/art.routes.js
+++ Backend/api/art/art.routes.js
@@ -3,9 +3,6 @@ const { requireArtist } = require('../../middlewares/requireAuth.middleware')
 const { getArts, getArt, deleteArt, updateArt, addArt } = require('./art.controller')
 const router = express.Router()
 
-// middleware that is specific to this router
-// router.use(requireAuth)
-
 
 router.get('/', getArts)
 router.get('/:id', getArt)
