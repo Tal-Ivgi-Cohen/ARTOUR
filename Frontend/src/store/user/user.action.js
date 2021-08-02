6b02072af6b3a9d39df817fff7f4dd2b6619243b
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-08-02 09:50:15 +0300

    Update user.action.js

--- Frontend/src/store/user/user.action.js
+++ Frontend/src/store/user/user.action.js
@@ -6,50 +6,48 @@ export function loadUsers() {
             const users = await userService.query();
             dispatch({ type: 'SET_USERS', users });
         } catch (err) {
-           // console.log('User Actions: err in loaded users', err);
+           console.log('User Actions: err in loaded users', err);
         }
     };
 }
+
 export function loadLoggedInUser() {
     return async dispatch => {
         try {
             const user = await userService.getLoggedInUser();
             if (user) {
                 dispatch({ type: 'SET_USER', user });
-               // console.log('load user', user._id);
             }
-
         } catch (err) {
-            //console.log('User Actions: err in loaded User', err);
+           console.log('User Actions: err in loaded User', err);
         }
     };
 }
-//LOGIN
+
 export function login(credentials) {
     return async dispatch => {
         try {
             const user = await userService.login(credentials);
-          //  console.log('user in reducer', user);
             if (user) {
                 dispatch({ type: 'LOGIN', user });
             }
         } catch (err) {
-           // console.log('User Actions: err in login', err);
+           console.log('User Actions: err in login', err);
         }
     };
 }
-//LOGOUT
+
 export function logout() {
     return dispatch => {
         try {
             userService.logout();
             dispatch({ type: 'LOGOUT' });
         } catch (err) {
-          //  console.log('User Actions: err in logout', err);
+          console.log('User Actions: err in logout', err);
         }
     };
 }
-//SIGNUP
+
 export function signup(userInfo) {
     return async dispatch => {
         try {
@@ -59,11 +57,11 @@ export function signup(userInfo) {
                 dispatch({ type: 'SIGNUP', user, users });
             }
         } catch (err) {
-            //console.log('User Actions: err in signUp', err);
+           console.log('User Actions: err in signUp', err);
         }
     };
 }
-//UPDATE
+
 export function updateUser(user) {
     return async dispatch => {
         try {
@@ -72,11 +70,11 @@ export function updateUser(user) {
                 dispatch({ type: 'UPDATE_USER', users, user });
             }
         } catch (err) {
-           //console.log('User Actions: err in updating a user', err);
+          console.log('User Actions: err in updating a user', err);
         }
     };
 }
-//RESET PASSWORD
+
 export function resetPassword(email, password) {
     return async dispatch => {
         try {
@@ -85,7 +83,7 @@ export function resetPassword(email, password) {
                 dispatch({ type: 'RESET_PASSWORD', users });
             }
         } catch (err) {
-          //  console.log('User Actions: err in resetting user password', err);
+          console.log('User Actions: err in resetting user password', err);
         }
     };
 }
