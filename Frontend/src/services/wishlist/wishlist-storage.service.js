16a1921623e64633729f0606225611e37ba0aa6f
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-08-02 09:45:50 +0300

    Update wishlist-storage.service.js

--- Frontend/src/services/wishlist/wishlist-storage.service.js
+++ Frontend/src/services/wishlist/wishlist-storage.service.js
@@ -8,18 +8,16 @@ export const storageService = {
   remove,
 };
 
-//READ LIST
 async function query(entityType) {
   let entities = await JSON.parse(localStorage.getItem(entityType)) || [];
-
   return entities;
 }
-//DETAILS FIND ONE BY ID
+
 async function get(entityType, entityId) {
   const entities = await query(entityType);
   return entities.find(entity => entity._id === entityId);
 }
-//ADD
+
 async function post(entityType, newEntity) {
   newEntity._id = utilService.makeId();
   const entities = await query(entityType);
@@ -27,16 +25,15 @@ async function post(entityType, newEntity) {
   _save(entityType, entities);
   return entities;
 }
-//UPDATE
+
 async function put(entityType, updatedEntity) {
-  //console.log('the art in storage service', updatedEntity);
   const entities = await query(entityType);
   const idx = entities.findIndex(entity => entity._id === updatedEntity._id);
   entities.splice(idx, 1, updatedEntity);
   _save(entityType, entities);
   return entities;
 }
-//DELETE
+
 async function remove(entityType, entityId) {
   const entities = await query(entityType);
   const idx = entities.findIndex(entity => entity._id === entityId);
@@ -44,7 +41,7 @@ async function remove(entityType, entityId) {
   _save(entityType, entities);
   return entities;
 }
-//SAVE TO STORAGE
+
 function _save(entityType, entities) {
   localStorage.setItem(entityType, JSON.stringify(entities));
 }
