fafa6b38a5c29d643f8dbedd9c8db5e2c1ef1857
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-08-02 09:47:34 +0300

    Update art.action.js

--- Frontend/src/store/art/art.action.js
+++ Frontend/src/store/art/art.action.js
@@ -6,18 +6,18 @@ export function loadArts(filterBy) {
         const arts = await artService.loadArts(filterBy);
             dispatch({ type: 'SET_ARTS', arts });
         } catch (err) {
-            //console.log('Art Actions: err in loaded Arts', err);
+            console.log('Art Actions: err in loaded Arts', err);
         }
     };
 }
-//REMOVE/DELETE
+
 export function removeArt(artId) {
     return async dispatch => {
         try {
             await artService.remove(artId);
             dispatch({ type: 'REMOVE_ART', artId });
         } catch (err) {
-           // console.log('ArtActions: err in removeArt', err);
+           console.log('ArtActions: err in removeArt', err);
         }
     };
 }
@@ -25,23 +25,20 @@ export function saveArt(art) {
     return async dispatch => {
         try {
             await artService.save(art)
-           // console.log('arts after update', art);
             dispatch({ type: art._id ? 'UPDATE_ART' : 'ADD_ART', art: art })
         } catch (err) {
-           // console.log(`ToysActions: err in ${art._id ? 'update art' : 'add art'}${err}`)
+           console.log(`ToysActions: err in ${art._id ? 'update art' : 'add art'}${err}`)
         }
     }
 }
-
 
 export function setArt(artId) {
     return async dispatch => {
         try {
             const art = await artService.getById(artId);
-           // console.log('art', art);
             dispatch({ type: 'SET_ART', art });
         } catch (err) {
-           // console.log('Art Actions: err in selected Art', err);
+           console.log('Art Actions: err in selected Art', err);
         }
     };
 }
