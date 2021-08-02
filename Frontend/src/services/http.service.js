6d6fc90a6870c2e9ce13ef42f6652a723a2600e1
Tal Cohen <81317982+Tal-Ivgi-Cohen@users.noreply.github.com>
2021-08-02 09:46:32 +0300

    Update http.service.js

--- Frontend/src/services/http.service.js
+++ Frontend/src/services/http.service.js
@@ -38,14 +38,9 @@ async function ajax(endpoint, method = 'GET', data = null) {
 
         return res.data
     } catch (err) {
-       // console.log(`Had Issues ${method}ing to the backend, endpoint: ${endpoint}, with data: ${data}`)
         console.dir(err)
         if (err.response && err.response.status === 401) {
-            // Depends on routing startegy - hash or history
             window.location.assign('/#/login')
-           // window.location.assign('/login')
-            //router.push('/login')
-            // }
             throw err
         }
     }
